a12a707cfdfad43a82e71db0aa833c9442a72814
Joonas Timlin <joonas@timlins.se>
2023-12-03 14:12:48 +0000

    Add search field to filters

--- posts/serializers.py
+++ posts/serializers.py
@@ -9,6 +9,8 @@ class PostSerializer(serializers.ModelSerializer):
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url') 
     like_id = serializers.SerializerMethodField()
+    comments_count = serializers.ReadOnlyField()
+    likes_count = serializers.ReadOnlyField()
 
     def validate_image(self, value):
         if value.size > 2 * 1024 * 1024:
@@ -41,5 +43,6 @@ class PostSerializer(serializers.ModelSerializer):
         model = Post
         fields = [
             'id', 'owner', 'is_owner', 'profile_id', 'profile_image',
-            'created_at', 'updated_at', 'title', 'content', 'image', 'image_filter', 'like_id'
+            'created_at', 'updated_at', 'title', 'content', 'image', 'image_filter', 'like_id',
+            'comments_count', 'likes_count'
         ]
